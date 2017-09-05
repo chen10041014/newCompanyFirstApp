9bd6dff88d5d27326e534ebd8ce71da34e36bc1d
d2z <123456>
2017-09-05 22:30:12 +0800

    添加了工程

---
+++ app/src/main/java/com/yuanjin/attorney/attorney/ui/activity/CaseDetailActivity.java
@@ -0,0 +1,21 @@
+package com.yuanjin.attorney.attorney.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yuanjin.attorney.attorney.R;
+
+public class CaseDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_case_detail);
+        init();
+    }
+
+    private void init() {
+
+
+    }
+}
